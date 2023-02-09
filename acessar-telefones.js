6c5599c5d4bef84ac3bcaf4dc531926038038965
William de Camargo Smolarek <111304963+willsmolarek@users.noreply.github.com>
2023-02-09 18:25:43 -0300

    Delete acessar-telefones.js

--- acessar-telefones.js
+++
@@ -1,9 +0,0 @@
-const cliente = {
-    nome:"Andre",
-    idade:36,
-    cpf:"12543652266",
-    email:"andre@gmail.com",
-    fones:["5591235498","5521988743124"]
-}
-
-cliente.fones.forEach(fone => console.log(fone))
